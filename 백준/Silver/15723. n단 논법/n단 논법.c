ae4adcff324037a64e1091923489982f1d8eed14
ketarubot <82302028+ketarubot@users.noreply.github.com>
2025-08-14 22:50:48 +0900

    [Silver I] Title: n단 논법, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/15723. n단 논법/n단 논법.c
+++ 백준/Silver/15723. n단 논법/n단 논법.c
@@ -1,32 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #define MAXLEN 26
 
+int matrix[MAXLEN][MAXLEN];
 int main(void) {
     int n;
     scanf("%d", &n);
-
-    int **arr = malloc(MAXLEN * sizeof(int *));
-    for (int i = 0; i < MAXLEN; i++) {
-        arr[i] = malloc(MAXLEN * sizeof(int));
-        for (int j = 0; j < MAXLEN; j++) {
-            arr[i][j] = 0;
-        }
-    }
-
+    
     for (int _ = 0; _ < n; _++) {
         char a, b;
         scanf("%*c%c is %c", &a, &b);
-        arr[a - 'a'][b - 'a'] = 1;
+        matrix[a - 'a'][b - 'a'] = 1;
     }
 
     for (int k = 0; k < MAXLEN; k++) {
         for (int i = 0; i < MAXLEN; i++) {
             for (int j = 0; j < MAXLEN; j++) {
-                if (arr[i][j]) continue;
-                if (arr[i][k] && arr[k][j]) arr[i][j] = 1;
-                else arr[i][j] = 0;
+                if (matrix[i][j]) continue;
+                if (matrix[i][k] && matrix[k][j]) matrix[i][j] = 1;
+                else matrix[i][j] = 0;
             }
         }
     }
@@ -36,11 +28,6 @@ int main(void) {
     for (int _ = 0; _ < m; _++) {
         char a, b;
         scanf("%*c%c is %c", &a, &b);
-        printf(arr[a-'a'][b-'a']?"T\n":"F\n");
+        printf(matrix[a-'a'][b-'a']?"T\n":"F\n");
     }
-
-    for (int i = 0; i < n; i++) {
-        free(arr[i]);
-    }
-    free(arr);
 }
