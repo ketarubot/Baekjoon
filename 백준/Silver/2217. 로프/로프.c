96c7b7e93275b0d567d0ff015e4e7876776eeefc
ketarubot <82302028+ketarubot@users.noreply.github.com>
2025-08-17 22:28:57 +0900

    [Silver IV] Title: 로프, Time: 24 ms, Memory: 1896 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10000
+
+int compare(const void *a, const void *b) {
+    return *(int*)a - *(int*)b;
+}
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    int* rope = malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &rope[i]);
+    }
+    qsort(rope, n, sizeof(int), compare);
+
+    int max_weight = 0;
+    for (int i = 0; i < n; i++) {
+        if (rope[i] * (n - i) > max_weight) {
+            max_weight = rope[i] * (n - i);
+        }
+    }
+    printf("%d\n", max_weight);
+}
