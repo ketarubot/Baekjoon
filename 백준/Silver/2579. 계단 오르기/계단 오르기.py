b0b709de7507ec4498407abf78c524c03145e8d2
ketarubot <82302028+ketarubot@users.noreply.github.com>
2025-08-24 21:52:22 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,11 @@
+n = int(input())
+scores = [0] + [int(input()) for _ in range(n)]
+dp = [0] * (n+1)
+
+dp[1] = scores[1]
+if n >= 2:
+    dp[2] = scores[1] + scores[2]
+    for i in range(3, n+1):
+        dp[i] = max(dp[i-2], dp[i-3] + scores[i-1]) + scores[i]
+
+print(dp[n])
