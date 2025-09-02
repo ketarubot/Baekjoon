365da408297bbb3219655690cb3da136e944760f
ketarubot <82302028+ketarubot@users.noreply.github.com>
2025-09-02 09:42:36 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 220 ms, Memory: 47188 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,14 +1,17 @@
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
 
 np = [""] * (n+1)
 pn = {}
 for i in range(1, n+1):
-    name = input()
+    name = input().strip()
     np[i] = name
     pn[name] = i
 
 for _ in range(m):
-    q = input()
+    q = input().strip()
     if q.isalpha():
         print(pn[q])
     else:
